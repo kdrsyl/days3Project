c065baf86be5f9ad7483a1532bf2853f92381fb8
kdrsyl <kadersayligb@gmail.com>
2020-10-07 21:54:09 +0100

    initial awesome committed new file for awesome feature

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -2,5 +2,9 @@ public class AwesomeTest {
 
     // red -> untracked
     //green -> under the staging area
+    //grey in .idea folder means it's ignored
+    // when the colour is normal, it means it's committed no changes
+
+
 
 }
