466d7bddd2361d297ee8f743e522d12150387417
kdrsyl <kadersayligb@gmail.com>
2020-10-07 22:06:38 +0100

    new lines added

--- src/test/java/AwesomeTest.java
+++ src/test/java/AwesomeTest.java
@@ -5,6 +5,11 @@ public class AwesomeTest {
     //grey in .idea folder means it's ignored
     // when the colour is normal, it means it's committed no changes
 
+    //I've just pulled the new change from GitHub that I did not have locally
+    // and now my project is in sync with remote repository
+
+    //now I made more awesome changes and I am about To push this back GitHub
+
 
 
 }
